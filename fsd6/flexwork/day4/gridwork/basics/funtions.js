43f3b15a48364fdc6e84dd007519dff04f96d4d9
your name <sachin.23b1541143@abes.ac.in>
2025-09-15 15:22:17 +0530

    4th commit

---
+++ fsd6/flexwork/day4/gridwork/basics/funtions.js
@@ -0,0 +1,13 @@
+// function fun() {
+//     console.log("best earth");
+// }
+// console.log(fun);
+// console.log(a());
+
+
+// const a = ()=>{
+//     console.log("best earth");
+// }
+// console.log(a);
+// console.log(a());
+
---
+++ fsd6/flexwork/day4/gridwork/index2.js
@@ -0,0 +1,8 @@
+let x = 5;
+function modify (val){
+    val = 10;
+}
+modify(x);
+console.log(x);
+
+int x = [2,3,4];
