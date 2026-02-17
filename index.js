af22a4c12a63319c3be90ee6663fc1095e68e343
“fsd” <sachin.23b1541143@abes.ac.in>
2026-02-17 11:35:19 +0530

    17/2b

--- index.js
+++ index.js
@@ -1,82 +1,94 @@
-const fs=require('fs');
-//node js is a server and backend
-// // sync(synchronous) and async(asynchronous
-// //sync execute all tesks one by one either it's take more time
-// for output write node index.js in terminal
-// //async execute all tasks parallelly it will take less time
-// //in async we use callback funtion to handle results
-// // fs.writeFileSync("./ds-b.txt","hello abes student");
-// // fs.writeFileSync("./abes.txt","hi abes college student");
+// const fs=require('fs');
+// //node js is a server and backend
+// // // sync(synchronous) and async(asynchronous
+// // //sync execute all tesks one by one either it's take more time
+// // for output write node index.js in terminal
+// // //async execute all tasks parallelly it will take less time
+// // //in async we use callback funtion to handle results
+// // // fs.writeFileSync("./ds-b.txt","hello abes student");
+// // // fs.writeFileSync("./abes.txt","hi abes college student");
 
-// // const result = fs.readFileSync("./abes.txt","utf-8");
+// // // const result = fs.readFileSync("./abes.txt","utf-8");
+// // // console.log(result);
+// // // const result1 = fs.readFileSync("./ds-b.txt","utf-8");
+// // // console.log(result1);
+
+// // // fs.writeFile("./abes.txt","we have a code", () => { });   //() =>{ } is callback funtion
+// // // fs.writeFile("./ds-b.txt","we are abes student", () => { });
+
+// // fs.readFile("./abes.txt","utf-8", (err,result)=> {  
+// // if(err)
+// // {
+// //     console.log("error",err);
+// // }
+// // else{
+// //     console.log(result);
+// // }
+// // });
+
+// //  fs.writeFileSync("./a1.txt","hello abes student");
+// //  fs.writeFileSync("./a2.txt","hi abes college student");
+// //   const result = fs.readFileSync("./a1.txt","utf-8");
 // // console.log(result);
-// // const result1 = fs.readFileSync("./ds-b.txt","utf-8");
+// // const result1 = fs.readFileSync("./a2.txt","utf-8");
 // // console.log(result1);
 
-// // fs.writeFile("./abes.txt","we have a code", () => { });   //() =>{ } is callback funtion
-// // fs.writeFile("./ds-b.txt","we are abes student", () => { });
-
-// fs.readFile("./abes.txt","utf-8", (err,result)=> {  
-// if(err)
-// {
-//     console.log("error",err);
-// }
-// else{
-//     console.log(result);
-// }
-// });
-
-//  fs.writeFileSync("./a1.txt","hello abes student");
-//  fs.writeFileSync("./a2.txt","hi abes college student");
-//   const result = fs.readFileSync("./a1.txt","utf-8");
-// console.log(result);
-// const result1 = fs.readFileSync("./a2.txt","utf-8");
-// console.log(result1);
-
 
  
-//  fs.writeFile("./b1.txt","we have a code", () => { });   //() =>{ } is callback funtion
-//  fs.writeFile("./b2.txt","we are abes student", () => { });
+// //  fs.writeFile("./b1.txt","we have a code", () => { });   //() =>{ } is callback funtion
+// //  fs.writeFile("./b2.txt","we are abes student", () => { });
 
-//  fs.readFile("./b1.txt","utf-8", (err,result)=> {  
-// if(err)
+// //  fs.readFile("./b1.txt","utf-8", (err,result)=> {  
+// // if(err)
+// // {
+// //     console.log("error",err);
+// // }
+// // else{
+// //     console.log(result);
+// // }
+// // });
+
+// // fs.readFile("./b2.txt","utf-8", (err,result)=> {  
+// // if(err)
+// // {
+// //     console.log("error",err);
+// // }
+// // else{
+// //     console.log(result);
+// // }
+// // });
+ 
+// //"./" -> current directory
+
+// // fs.appendFileSync("./abes.txt", `phd`);
+
+// // fs.appendFile("./abes.txt",`phd`, () => { });   //async
+
+// // fs.cpSync("./abes.txt","./c1.txt");
+
+// // fs.cp("./abes.txt","./c2.txt", () => { });    //async
+
+// // fs.unlinkSync("./c1.txt"); //delete file by sync
+
+// fs.unlink("./c2.txt",(err) => {
+//     if(err)                           //delete file by async
 // {
-//     console.log("error",err);
-// }
+//      console.log("error",err);
+//  }
 // else{
-//     console.log(result);
-// }
+//      console.log("successfully");
+//  }
+ 
 // });
 
-// fs.readFile("./b2.txt","utf-8", (err,result)=> {  
-// if(err)
-// {
-//     console.log("error",err);
-// }
-// else{
-//     console.log(result);
-// }
-// });
- 
-//"./" -> current directory
+//os module is used to get information about operating system
 
-// fs.appendFileSync("./abes.txt", `phd`);
-
-// fs.appendFile("./abes.txt",`phd`, () => { });   //async
-
-// fs.cpSync("./abes.txt","./c1.txt");
-
-// fs.cp("./abes.txt","./c2.txt", () => { });    //async
-
-// fs.unlinkSync("./c1.txt"); //delete file by sync
-
-fs.unlink("./c2.txt",(err) => {
-    if(err)                           //delete file by async
-{
-     console.log("error",err);
- }
-else{
-     console.log("successfully");
- }
- 
-});
+const os=require('os');
+console.log("system.platform",os.platform());  //to get platform of system architecture information
+console.log("user Info",os.userInfo());
+console.log("cpu",os.arch());
+console.log("free memory",os.freemem());
+console.log("total memory",os.totalmem());
+console.log("uptime",os.uptime());
+console.log("home directory",os.homedir());
+console.log("host name",os.hostname());
